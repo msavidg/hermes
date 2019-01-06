d2bb83a2514ebe07e2f0e1a2bd63d422c9dc254f
msavidg <msavidg@gmail.com>
2019-01-06 12:47:37 -0500

    Added self-hosted web

---
+++ Hermes.EndpointLoadBalancer.Service/OwinStartup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Owin;
+
+
+//https://github.com/danesparza/OWIN-WebAPI-Service
+
+//https://docs.microsoft.com/en-us/aspnet/web-api/overview/hosting-aspnet-web-api/use-owin-to-self-host-web-api
+
+namespace Hermes.EndpointLoadBalancer.Service
+{
+    public class OwinStartup
+    {
+        //  Hack from http://stackoverflow.com/a/17227764/19020 to load controllers in 
+        //  another assembly.  Another way to do this is to create a custom assembly resolver
+        Type valuesControllerType = typeof(Controllers.ConfigController);
+
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            // Configure Web API for self-host. 
+            HttpConfiguration config = new HttpConfiguration();
+
+            //  Enable attribute based routing
+            //  http://www.asp.net/web-api/overview/web-api-routing-and-actions/attribute-routing-in-web-api-2
+            config.MapHttpAttributeRoutes();
+
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{action}/{id}",
+            //    defaults: new { controller = "config", action="gettime", id = RouteParameter.Optional }
+            //);
+
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
