771743c3cc29e122736abc66eae970228cecadbe
Michael J Savidge <msavidge@navg.com>
2019-01-14 22:26:42 -0500

    Work checkin

--- Hermes.EndpointLoadBalancer.Service/Controllers/ConfigController.cs
+++ Hermes.EndpointLoadBalancer.Service/Controllers/ConfigController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
@@ -10,11 +12,28 @@ namespace Hermes.EndpointLoadBalancer.Service.Controllers
     [RoutePrefix("api/Config")]
     public class ConfigController : ApiController
     {
-        [Route("~/",Name="default")]
+
+
+        public ConfigController()
+        {
+        }
+
+        [Route("~/", Name = "default")]
         [Route("GetTime")]
         public string GetTime()
         {
             return DateTime.Now.ToString("O");
+        }
+
+        [Route("ShowRegisteredEndpoints")]
+        public HttpResponseMessage ShowRegisteredEndpoints()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+
+            HttpResponseMessage httpResponseMessage = Request.CreateResponse(HttpStatusCode.OK);
+            httpResponseMessage.Content = new StringContent(stringBuilder.ToString(), Encoding.UTF8, "text/html");
+
+            return httpResponseMessage;
         }
     }
 }
