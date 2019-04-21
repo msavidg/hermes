106de5889df396494920ca9337285b3a53e97974
Michael J Savidge <msavidge@navg.com>
2019-04-21 09:35:48 -0400

    Stubbed out OCR message handler.

---
+++ OCRMessageHandler/OCRMessageHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NServiceBus;
+using OCRMessageInterface;
+
+namespace OCRMessageHandler
+{
+    public class OCRMessageHandler : IHandleMessages<IOCRMessage>
+    {
+        public Task Handle(IOCRMessage message, IMessageHandlerContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
